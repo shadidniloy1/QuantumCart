c6e55d6d36535e821b434a73ebd95145d316fe4b
Shadid Hasan <shadidhasan1@gmail.com>
2026-04-25 00:06:34 +0600

    shop page is fixed, now functioning properly!

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,37 +1,3 @@
-// import { PrismaClient } from "../app/generated/prisma/client"; 
-// import { PrismaPg } from "@prisma/adapter-pg"; 
-// const globalForPrisma = global as unknown as {
-//   prisma: PrismaClient; 
-// }; 
-// const adapter = new PrismaPg({
-//   connectionString: process.env.DATABASE_URL, 
-// }); 
-// const prisma =
-//   globalForPrisma.prisma ||
-//   new PrismaClient({
-//     adapter, 
-//   }); 
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
-// export default prisma; 
-
-// import { PrismaClient } from "../app/generated/prisma";
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClient | undefined;
-// };
-
-// const prisma =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({
-//     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-//   });
-
-// if (process.env.NODE_ENV !== "production") {
-//   globalForPrisma.prisma = prisma;
-// }
-
-// export default prisma;
-
 import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
